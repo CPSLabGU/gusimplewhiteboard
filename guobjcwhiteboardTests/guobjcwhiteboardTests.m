dc3ad64bf310f3141d56b805ff108296820f0331
Rene Hexel <r.hexel@griffith.edu.au>
2012-05-06 07:20:21 +1000

    Add an ObjCWhiteboard class for accessing the whiteboard

---
+++ guobjcwhiteboardTests/guobjcwhiteboardTests.m
@@ -0,0 +1,32 @@
+//
+//  guobjcwhiteboardTests.m
+//  guobjcwhiteboardTests
+//
+//  Created by René Hexel on 6/05/12.
+//  Copyright (c) 2012 Rene Hexel. All rights reserved.
+//
+
+#import "guobjcwhiteboardTests.h"
+
+@implementation guobjcwhiteboardTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in guobjcwhiteboardTests");
+}
+
+@end
