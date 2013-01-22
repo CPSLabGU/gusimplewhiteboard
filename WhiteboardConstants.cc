2b503c89beb981f31eec649f5eb7d1ba7dd859ba
Vlad Estivill-Castro <vlad@ubuntu64.(none)>
2013-01-23 09:40:48 +1000

    Works except lights for 64 bits, docuemntation gives no serious warnings, but does not build WhiteboardConstants

--- WhiteboardConstants.cc
+++ WhiteboardConstants.cc
@@ -40,9 +40,11 @@ namespace guWhiteboard
 	const char *kOn = "On";
 	const char *kOff = "Off";
 	
+	// LONG GONE: use MotorPower and an ARRAY
 	//const char *kDifferentialMotorForward = "gudifferential_motorForward";
 	//const char *kDifferentialMotorBackward = "gudifferential_motorBackward";
 	//const char *kDifferentialMotorStop = "gudifferential_motorStop";
+
 	/*!
          * This is the new message-type for controlling a motor
 	 * content type is ALWAYS ARRAY
@@ -66,19 +68,70 @@ namespace guWhiteboard
 	 * */
 	const char *kDifferentialStopSound = "gudifferential_stopSound";
 
+
+	/**Differential Robot message to stop a touch sensor reporting
+	 * Content (Int): Id of touch sensor for NXT(1, 2, 3 or 4)
+	 * */
 	const char *kDifferentialStopTouch = "gudifferential_stopTouchSensor";
+	/**Differential Robot message to start a touch sensor reporting
+	 * Content (Int): Id of touch sensor for NXT(1, 2, 3 or 4)
+	 * */
 	const char *kDifferentialStartTouch = "gudifferential_startTouchSensor";
+
+	/**Differential Robot Whiteboard message reporting the value of a
+	*  touch sensor, the message type is appended with an active
+	* Id of touch sensor for NXT(1, 2, 3 or 4)
+	* content is the value of sensor, 0 if released, 1 if pressed
+        **/
 	const char *kDifferentialTouchValue = "gudifferential_touchSensorValue";
+
+
+	/**Differential Robot message to start a sonar sensor reporting
+	 * Content (Int): Id of touch sensor for NXT(1, 2, 3 or 4)
+	 * */
 	const char *kDifferentialStartDistance = "gudifferential_startDistanceSensor";
+
+	/**Differential Robot message to STOP a sonar sensor reporting
+	 * Content (Int): Id of touch sensor for NXT(1, 2, 3 or 4)
+	 * */
 	const char *kDifferentialStopDistance = "gudifferential_stopDistanceSensor";
+
+	/**Differential Robot Whiteboard message reporting the value of a
+	*  sonar sensor, the message type is appended with an active
+	* Id of touch sensor for NXT(1, 2, 3 or 4)
+	* content is the value of sensor, in cm
+        **/
 	const char *kDifferentialDistanceValue = "gudifferential_distanceSensorValue";
+
+
+	/**Differential Robot message to start a rotation sensor reporting
+	 * Content (Int): Id of relevant motor
+	 * Id of motor 0=left, 1=right 2 third
+	 * for MiPAL NXT wired robots, C=left adn B= right
+        **/
+	const char *kDifferentialStartRotationSensor = "gudifferential_startRotationSensor";
+
+	/**Differential Robot message to STOP a rotation sensor reporting
+	 * Content (Int): Id of relevant motor
+	 * Id of motor 0=left, 1=right 2 third
+	 * for MiPAL NXT wired robots, C=left adn B= right
+        **/
+	const char *kDifferentialStopRotationSensor = "gudifferential_stopRotationSensor";
+
+	/**Differential Robot Whiteboard message reporting the value of a
+	*  rotation on a motor, the message type is appended with an active
+	* Id of motor 0=left, 1=right 2 third
+	* for MiPAL NXT wired robots, C=left adn B= right
+	* content is the value of sensor, in laps
+        **/
+	const char *kDifferentialRotationValue = "gudifferential_rotationSensorValue";
+
 	const char *kDifferentialStartLight = "gudifferential_startLightSensor";
 	const char *kDifferentialStopLight = "gudifferential_stopLightSensor";
 	const char *kDifferentialLightValue = "gudifferential_lightSensorValue";
+
+
 	const char *kDifferentialLedOn = "gudifferential_ledOn";
 	const char *kDifferentialLedOff = "gudifferential_ledOff";
-	const char *kDifferentialStartRotationSensor = "gudifferential_startRotationSensor";
-	const char *kDifferentialStopRotationSensor = "gudifferential_stopRotationSensor";
-	const char *kDifferentialRotationValue = "gudifferential_rotationSensorValue";
 	
 }
