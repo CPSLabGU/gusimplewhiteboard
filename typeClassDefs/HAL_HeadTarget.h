d96edf94171c22bda9f1ee645b1fbf1544f77daf
Carl <carl.lusty@griffithuni.edu.au>
2013-03-21 15:08:42 +1000

    Added a PROPERTY hash def into the util class for generating C++ getters and setters automatically. Changed the HAL_Head class to use this and updated motion to the HAL_Head interface change (ie. to use the getters / setters).

--- typeClassDefs/HAL_HeadTarget.h
+++ typeClassDefs/HAL_HeadTarget.h
@@ -10,7 +10,7 @@
 #ifndef HAL_HeadTarget_DEFINED                                       
 #define HAL_HeadTarget_DEFINED
 
-#include <vector>
+#include <gu_util.h>
 
 namespace guWhiteboard                                                  
 {
@@ -22,27 +22,26 @@ namespace guWhiteboard
         
         class HAL_HeadTarget
         {
-        public:
-                
                 //Angles are in degrees
-                int target_pitchAngle; //not sure, look it up in the AL_Docs
-                int target_yawAngle; //-90 to 90
+                PROPERTY(int, target_pitchAngle); //not sure, look it up in the AL_Docs
+                PROPERTY(int, target_yawAngle); //-90 to 90
                 
-                movement_type target_movement_type; //Move over time or with a percentage of max speed
+                PROPERTY(movement_type, target_movement_type); //Move over time or with a percentage of max speed
                 
                 //One of these values will be ignored base on the movement_type selected
-                int target_movement_time; //usec
-                int target_movement_speed; //percentage of a predefined max speed (potentially slower than moving with time, but a good deal safer)
-                bool head_stopped; //Will be true if the head is not moving, will also stop the head if set on the WB
-                
+                PROPERTY(int, target_movement_time); //usec
+                PROPERTY(int, target_movement_speed); //percentage of a predefined max speed (potentially slower than moving with time, but a good deal safer)
+                PROPERTY(bool, head_stopped); //Will be true if the head is not moving, will also stop the head if set on the WB
+
+        public:                
                 HAL_HeadTarget(int target_pitchAngle = 0, int target_yawAngle = 0, movement_type target_movement_type = MoveOverTime, int target_movement_time = 1000000, int target_movement_speed = 50, bool head_stopped = true)
                 {
-                        this->target_pitchAngle = target_pitchAngle;
-                        this->target_yawAngle = target_yawAngle;
-                        this->target_movement_speed = (movement_type)target_movement_type;
-                        this->target_movement_time = target_movement_time;
-                        this->target_movement_speed = target_movement_speed;
-                        this->head_stopped = head_stopped;
+                        this->target_pitchAngle(target_pitchAngle);
+                        this->target_yawAngle(target_yawAngle);
+                        this->target_movement_speed(target_movement_type);
+                        this->target_movement_time(target_movement_time);
+                        this->target_movement_speed(target_movement_speed);
+                        this->head_stopped(head_stopped);
                 }
         };
 }
