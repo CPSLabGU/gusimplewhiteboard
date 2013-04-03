ba677eab3e31446faa197f8fa09de4d7a34568f9
Carl <carl.lusty@griffithuni.edu.au>
2013-04-03 14:05:42 +1000

    Added a conversion getter method to go from rad to deg for joint values.

--- typeClassDefs/SENSORS_LegJointSensors.h
+++ typeClassDefs/SENSORS_LegJointSensors.h
@@ -10,18 +10,26 @@
 #ifndef SENSORS_LegJointSensors_DEFINED
 #define SENSORS_LegJointSensors_DEFINED
 
+#define _USE_MATH_DEFINES
 #include <gu_util.h>
+#include <cmath>
+
+
+#define JOINT_DEG_GETTER(t, n) public: t n##_degrees () { return (_##n * (180/M_PI)) ; }
+#define JOINT_PROPERTY(type, name) PROPERTY_DECLARATION(type,name); PROPERTY_GETTER(type,name) JOINT_DEG_GETTER(type,name) PROPERTY_SETTER(type,name)
+
+
 
 namespace guWhiteboard                                                  
 {
         class SENSORS_LegJointSensors
         {
-                PROPERTY(float, LKneePitch);
-                PROPERTY(float, LAnklePitch);
-                PROPERTY(float, LAnkleRoll);
-                PROPERTY(float, RKneePitch);
-                PROPERTY(float, RAnklePitch);
-                PROPERTY(float, RAnkleRoll);
+                JOINT_PROPERTY(float, LKneePitch);
+                JOINT_PROPERTY(float, LAnklePitch);
+                JOINT_PROPERTY(float, LAnkleRoll);
+                JOINT_PROPERTY(float, RKneePitch);
+                JOINT_PROPERTY(float, RAnklePitch);
+                JOINT_PROPERTY(float, RAnkleRoll);
                 
         public:
                 SENSORS_LegJointSensors()
--- typeClassDefs/SENSORS_TorsoJointSensors.h
+++ typeClassDefs/SENSORS_TorsoJointSensors.h
@@ -10,7 +10,15 @@
 #ifndef SENSORS_TorsoJointSensors_DEFINED
 #define SENSORS_TorsoJointSensors_DEFINED
 
+#define _USE_MATH_DEFINES
 #include <gu_util.h>
+#include <cmath>
+
+
+#define JOINT_DEG_GETTER(t, n) public: t n##_degrees () { return (_##n * (180/M_PI)) ; }
+#define JOINT_PROPERTY(type, name) PROPERTY_DECLARATION(type,name); PROPERTY_GETTER(type,name) JOINT_DEG_GETTER(type,name) PROPERTY_SETTER(type,name)
+
+
 #include <string>
 #include <sstream>
 
@@ -18,21 +26,21 @@ namespace guWhiteboard
 {
         class SENSORS_TorsoJointSensors
         {
-                PROPERTY(float, HeadYaw);
-                PROPERTY(float, HeadPitch);
-                PROPERTY(float, LShoulderPitch);
-                PROPERTY(float, LShoulderRoll);
-                PROPERTY(float, LElbowYaw);
-                PROPERTY(float, LElbowRoll);
-                PROPERTY(float, LHipYawPitch);
-                PROPERTY(float, LHipRoll);
-                PROPERTY(float, LHipPitch);
-                PROPERTY(float, RHipRoll);
-                PROPERTY(float, RHipPitch);
-                PROPERTY(float, RShoulderPitch);
-                PROPERTY(float, RShoulderRoll);
-                PROPERTY(float, RElbowYaw);
-                PROPERTY(float, RElbowRoll);
+                JOINT_PROPERTY(float, HeadYaw);
+                JOINT_PROPERTY(float, HeadPitch);
+                JOINT_PROPERTY(float, LShoulderPitch);
+                JOINT_PROPERTY(float, LShoulderRoll);
+                JOINT_PROPERTY(float, LElbowYaw);
+                JOINT_PROPERTY(float, LElbowRoll);
+                JOINT_PROPERTY(float, LHipYawPitch);
+                JOINT_PROPERTY(float, LHipRoll);
+                JOINT_PROPERTY(float, LHipPitch);
+                JOINT_PROPERTY(float, RHipRoll);
+                JOINT_PROPERTY(float, RHipPitch);
+                JOINT_PROPERTY(float, RShoulderPitch);
+                JOINT_PROPERTY(float, RShoulderRoll);
+                JOINT_PROPERTY(float, RElbowYaw);
+                JOINT_PROPERTY(float, RElbowRoll);
                 
         public:
                 SENSORS_TorsoJointSensors()
@@ -40,10 +48,11 @@ namespace guWhiteboard
 
                 }
                 
+                //temp method, testing only, will be removed soon - Carl.
                 std::string description()
                 {
                         std::stringstream ss;
-                        ss << _HeadYaw << ", " << _HeadPitch << ", " << _LShoulderPitch << ", " << _LShoulderRoll << ", " << _LElbowYaw << ", " << _LElbowRoll << ", " << _LHipYawPitch << ", " << _LHipRoll << ", " << _LHipPitch << ", " << _RHipRoll << ", " << _RHipPitch << ", " << _RShoulderPitch << ", " << _RShoulderRoll << ", " << _RElbowYaw << ", " << _RElbowRoll;
+                        ss << HeadYaw_degrees() << ", " << HeadPitch_degrees() << ", " << LShoulderPitch_degrees() << ", " << LShoulderRoll_degrees() << ", " << LElbowYaw_degrees() << ", " << LElbowRoll_degrees() << ", " << LHipYawPitch_degrees() << ", " << LHipRoll_degrees() << ", " << LHipPitch_degrees() << ", " << RHipRoll_degrees() << ", " << RHipPitch_degrees() << ", " << RShoulderPitch_degrees() << ", " << RShoulderRoll_degrees() << ", " << RElbowYaw_degrees() << ", " << RElbowRoll_degrees();
                         return ss.str();
                 }
         };
