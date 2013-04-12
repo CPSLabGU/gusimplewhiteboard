7f36a2580839ab4e22a609a60cf38e0694280edf
Rene Hexel <r.hexel@griffith.edu.au>
2013-04-13 07:33:36 +1000

    Turn some macros into inline and fix some compiler warnings

--- typeClassDefs/NAO_State.h
+++ typeClassDefs/NAO_State.h
@@ -26,8 +26,8 @@ namespace guWhiteboard
                 Knitting,       //NYI to be implemented by Rene
                 NUM_OF_STANCES
         };
-        
-        const std::string Robot_Stance_stringValues[NUM_OF_STANCES] =
+
+        static const char *Robot_Stance_stringValues[NUM_OF_STANCES] =
         {
                 "Standing",
                 "FallenForward",
@@ -38,8 +38,8 @@ namespace guWhiteboard
                 "Kneeling",     //NYI
                 "Knitting"      //NYI to be implemented by Rene
         };
-        
-        
+
+
         class NAO_State //NOTE: Read, Change, Write operations in state machines will not result in a race condition here. clfsm evaluates and runs states synchronously
         {
                 PROPERTY(Robot_Stance, Stance) //Currently SMRobotPosition detects if the robot has fallen over. If not fallen over SMRobotPosition posts Standing.
@@ -49,7 +49,6 @@ namespace guWhiteboard
                 PROPERTY(bool, Left_Foot_Pressed) //This is the fudged version of this information, it lasts for half a second instead of the actual duration of the push event (which is provided by sensors) - Updated by SMLeftFootButton
                 
                 PROPERTY(bool, Right_Foot_Pressed) //This is the fudged version of this information, it lasts for half a second instead of the actual duration of the push event (which is provided by sensors) - Updated by SMRightFootButton
-                
         public:
                 NAO_State()
                 {
@@ -64,7 +63,7 @@ namespace guWhiteboard
                 {
                         std::stringstream ss;
 
-                        ss << "Stance: " << Robot_Stance_stringValues[(int)Stance()] << ", Chest_Pressed: " << Chest_Pressed() << ", Left_Foot_Pressed: " << Left_Foot_Pressed() << ", Right_Foot_Pressed: " << Right_Foot_Pressed() << ";";
+                        ss << "Stance: " << Robot_Stance_stringValues[int(Stance())] << ", Chest_Pressed: " << Chest_Pressed() << ", Left_Foot_Pressed: " << Left_Foot_Pressed() << ", Right_Foot_Pressed: " << Right_Foot_Pressed() << ";";
                         
                         return ss.str();
                 }
