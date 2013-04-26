c37de4b08728992a71b4b0940c9abb0431e0b117
Carl <carl.lusty@griffithuni.edu.au>
2013-04-26 13:18:23 +1000

    First attempt at fixing the Head motions. May not be perfect.

--- typeClassDefs/HAL_HeadTarget.h
+++ typeClassDefs/HAL_HeadTarget.h
@@ -20,6 +20,19 @@ namespace guWhiteboard
                 MoveWithSpeed
         };
         
+        namespace HeadSpeed
+        {
+                enum head_speed
+                {
+                        One = 2, //2 is intentional
+                        Two,
+                        Three,
+                        Four,
+                        Five,
+                        MaxSpeed
+                };
+        }
+        
         class HAL_HeadTarget
         {
                 //Angles are in degrees
@@ -32,16 +45,44 @@ namespace guWhiteboard
                 PROPERTY(int, target_movement_time) ///< usec
                 PROPERTY(int, target_movement_speed) ///< percentage of a predefined max speed (potentially slower than moving with time, but a good deal safer)
                 PROPERTY(bool, head_stopped) ///< Will be true if the head is not moving, will also stop the head if set on the WB
-
+                
+                BITPROPERTY(head_cmd_mask)
         public:                
                 HAL_HeadTarget(int target_pitchAngle = 0, int target_yawAngle = 0, movement_type target_movement_type = MoveOverTime, int target_movement_time = 1000000, int target_movement_speed = 50, bool head_stopped = true)
                 {
                         set_target_pitchAngle(target_pitchAngle);
                         set_target_yawAngle(target_yawAngle);
-                        set_target_movement_speed(target_movement_type);
+                        set_target_movement_type(target_movement_type);
                         set_target_movement_time(target_movement_time);
                         set_target_movement_speed(target_movement_speed);
                         set_head_stopped(head_stopped);
+                        set_head_cmd_mask(false);
+                }
+                
+                void Stop()
+                {
+                        set_head_stopped(true);
+                        set_head_cmd_mask(true);
+                }
+                
+                void GoToWithTime(int yaw, int pitch, int time)
+                {
+                        set_target_pitchAngle(pitch);
+                        set_target_yawAngle(yaw);
+                        set_target_movement_time(time);
+                        set_head_stopped(false);
+                        set_target_movement_type(MoveOverTime);
+                        set_head_cmd_mask(true);
+                }
+                
+                void GoToWithSpeed(int yaw, int pitch, HeadSpeed::head_speed speed)
+                {
+                        set_target_pitchAngle(pitch);
+                        set_target_yawAngle(yaw);
+                        set_target_movement_speed((int)speed);
+                        set_head_stopped(false);
+                        set_target_movement_type(MoveWithSpeed);
+                        set_head_cmd_mask(true);
                 }
         };
 }
