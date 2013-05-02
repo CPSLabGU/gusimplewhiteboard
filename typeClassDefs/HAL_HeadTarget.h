968cdf13dc63138d03d8c8eeaffbd08c54883c4a
Carl Lusty <you@example.com>
2013-05-02 16:43:17 +1000

    Added rad methods to HAL_Head, updated the tracker machine, added rad2deg conversion methods, updated the workspace to remove an old WALK file, removed the move-with-speed option from motion.

--- typeClassDefs/HAL_HeadTarget.h
+++ typeClassDefs/HAL_HeadTarget.h
@@ -14,47 +14,24 @@
 
 namespace guWhiteboard                                                  
 {
-        enum movement_type
-        {
-                MoveOverTime = 0,
-                MoveWithSpeed
-        };
-        
-        namespace HeadSpeed
-        {
-                enum head_speed
-                {
-                        One = 2, //2 is intentional
-                        Two,
-                        Three,
-                        Four,
-                        Five,
-                        MaxSpeed
-                };
-        }
-        
         class HAL_HeadTarget
         {
                 //Angles are in degrees
-                PROPERTY(int, target_pitchAngle) ///< not sure, look it up in the AL_Docs
-                PROPERTY(int, target_yawAngle) ///< -90 to 90
-                
-                PROPERTY(movement_type, target_movement_type) ///< Move over time or with a percentage of max speed
-                
+                PROPERTY(float, target_pitchAngle) ///< not sure, look it up in the AL_Docs
+                PROPERTY(float, target_yawAngle) ///< 
+            
                 //One of these values will be ignored base on the movement_type selected
                 PROPERTY(int, target_movement_time) ///< usec
-                PROPERTY(int, target_movement_speed) ///< percentage of a predefined max speed (potentially slower than moving with time, but a good deal safer)
-                PROPERTY(bool, head_stopped) ///< Will be true if the head is not moving, will also stop the head if set on the WB
+                BITPROPERTY(head_stopped) ///< Will be true if the head is not moving, will also stop the head if set on the WB
                 
                 BITPROPERTY(head_cmd_mask)
-        public:                
-                HAL_HeadTarget(int target_pitchAngle = 0, int target_yawAngle = 0, movement_type target_movement_type = MoveOverTime, int target_movement_time = 1000000, int target_movement_speed = 50, bool head_stopped = true)
+        public:
+            
+                HAL_HeadTarget(float target_pitchAngle = 0, float target_yawAngle = 0, int target_movement_time = 1000000, bool head_stopped = true)
                 {
                         set_target_pitchAngle(target_pitchAngle);
                         set_target_yawAngle(target_yawAngle);
-                        set_target_movement_type(target_movement_type);
                         set_target_movement_time(target_movement_time);
-                        set_target_movement_speed(target_movement_speed);
                         set_head_stopped(head_stopped);
                         set_head_cmd_mask(false);
                 }
@@ -64,24 +41,18 @@ namespace guWhiteboard
                         set_head_stopped(true);
                         set_head_cmd_mask(true);
                 }
-                
-                void GoToWithTime(int yaw, int pitch, int time)
+
+                void GoToWithTime(float yaw, float pitch, int time = 1000000)
+                {
+                    GoToWithTime_rad(DEG2RAD(yaw), DEG2RAD(pitch), time);
+                }
+            
+                void GoToWithTime_rad(float yaw, float pitch, int time = 1000000)
                 {
                         set_target_pitchAngle(pitch);
                         set_target_yawAngle(yaw);
                         set_target_movement_time(time);
                         set_head_stopped(false);
-                        set_target_movement_type(MoveOverTime);
-                        set_head_cmd_mask(true);
-                }
-                
-                void GoToWithSpeed(int yaw, int pitch, HeadSpeed::head_speed speed)
-                {
-                        set_target_pitchAngle(pitch);
-                        set_target_yawAngle(yaw);
-                        set_target_movement_speed((int)speed);
-                        set_head_stopped(false);
-                        set_target_movement_type(MoveWithSpeed);
                         set_head_cmd_mask(true);
                 }
         };
