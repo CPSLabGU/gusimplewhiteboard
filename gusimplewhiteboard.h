4ff4a188fef60d62a66b7aea09a6c181b1fb200e
Rene Hexel <r.hexel@griffith.edu.au>
2013-06-02 18:58:32 +1000

    Fix some clang/c++11 issues

--- gusimplewhiteboard.h
+++ gusimplewhiteboard.h
@@ -71,7 +71,11 @@ extern "C"
 #endif
 #include <dispatch/dispatch.h>
 #include <sys/types.h>
+#undef __block
+#define __block _xblock
 #include <unistd.h>
+#undef __block
+#define __block __attribute__((__blocks__(byref)))
 
 #ifdef __cplusplus
 
