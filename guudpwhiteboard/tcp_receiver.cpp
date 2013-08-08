440326255da4a5e20a1fc62329f33e3d1a8a97b6
Carl Lusty <you@example.com>
2013-08-08 16:12:08 +1000

    Removed debug output

--- guudpwhiteboard/tcp_receiver.cpp
+++ guudpwhiteboard/tcp_receiver.cpp
@@ -136,7 +136,7 @@ void *connection(void *args)
 {
         int cfd = *(int *)args;
 
-        fprintf(stderr, "Connected!\n");
+//        fprintf(stderr, "Connected!\n");
         bool socket_open = true;
         while(socket_open)
         {
@@ -145,17 +145,17 @@ void *connection(void *args)
                 do
                 {
                         n += recv(cfd, &buff, sizeof(buff), 0);
-                        fprintf(stderr, "Reading... %d of %d\n", (int)n, (int)sizeof(buff));
+//                        fprintf(stderr, "Reading... %d of %d\n", (int)n, (int)sizeof(buff));
 
                 } while ((int)n < (int)sizeof(buff) && n > 0);
 
-                fprintf(stderr, "Got Msg\n");
+//                fprintf(stderr, "Got Msg\n");
                 if(n <= 0)
                         socket_open = false;
                 else
                         addToWB((uint16_t)buff.t, &buff.m);
         }
-        fprintf(stderr, "Disconnected!\n");
+//        fprintf(stderr, "Disconnected!\n");
 
         return NULL;
 }
