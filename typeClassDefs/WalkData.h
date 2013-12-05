6377991bad93060674755fd9be2bad61eae97ad3
Eugene Gilmore <eugene.gilmore@griffithuni.edu.au>
2013-12-05 19:07:01 +1000

    comment out posting of walk data, still wip

--- typeClassDefs/WalkData.h
+++ typeClassDefs/WalkData.h
@@ -4,6 +4,7 @@
  *
  * Created on 5 December 2013, 11:19 AM
  */
+#include <string>
 
 #ifndef WalkData_DEFINED
 #define	WalkData_DEFINED
@@ -12,8 +13,16 @@ namespace guWhiteboard {
 
 class WalkData {
 public:
-	float data[5][3];
+	float data[4][3];
 	int numPoints;
+	WalkData() {};
+	WalkData(std::string) {
+		for(int i = 0; i<3; ++i) {
+			for(int j = 0; j<4; ++j) {
+				data[j][i] = i*j;
+			}
+		}
+	}
 };
 }
 
