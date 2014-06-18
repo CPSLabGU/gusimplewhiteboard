0663bc397554b4f2ccac7c613aa87193a82ed8ba
Carl Lusty <carl.lusty@griffithuni.edu.au>
2014-06-18 16:34:44 +1000

    Updated nao_state

--- typeClassDefs/NAO_State.h
+++ typeClassDefs/NAO_State.h
@@ -5,32 +5,20 @@
  *  Copyright (c) 2013 Carl Lusty                                       
  *  All rights reserved.                                                
  */                                                                     
-
                                                                         
 #ifndef NAO_State_DEFINED
 #define NAO_State_DEFINED
 
 #include <sys/types.h>
-#include <gu_util.h>
 #include <sstream>
 #ifdef WHITEBOARD_POSTER_STRING_CONVERSION
 #include <ctype.h>
 #endif
 
+#include "wb_nao_state.h"
+
 namespace guWhiteboard                                                  
 {
-        enum Robot_Stance {
-                Standing = 0,
-                FallenForward,
-                FallenBack,
-                FallenLeft,
-                FallenRight,
-                Sitting,        //NYI
-                Kneeling,       //NYI
-                Knitting,       //NYI to be implemented by Rene
-                NUM_OF_STANCES
-        };
-
 #ifdef WHITEBOARD_POSTER_STRING_CONVERSION
         static const char *Robot_Stance_stringValues[NUM_OF_STANCES] =
         {
@@ -43,15 +31,7 @@ namespace guWhiteboard
                 "Kneeling",     //NYI
                 "Knitting"      //NYI to be implemented by Rene
         };
-#endif
 
-        enum Robot_Walk{
-                Modded_UNSW_Walk = 0,
-                ALMotion_Walk,
-                NUM_OF_WALKS
-        };
-
-#ifdef WHITEBOARD_POSTER_STRING_CONVERSION
         static const char *Robot_Walk_stringValues[NUM_OF_WALKS] =
         {
                 "Modded_UNSW_Walk",
@@ -59,25 +39,16 @@ namespace guWhiteboard
         };
 #endif
 
-        class NAO_State //NOTE: Read, Change, Write operations in state machines will not result in a race condition here. clfsm evaluates and runs states synchronously
+        class NAO_State : public wb_nao_state //NOTE: Read, Change, Write operations in state machines will not result in a race condition here. clfsm evaluates and runs states synchronously
         {
-                PROPERTY(Robot_Stance, stance)          ///< Currently SMRobotPosition detects if the robot has fallen over. If not fallen over SMRobotPosition posts Standing.
-                PROPERTY(Robot_Walk, walk)          ///< Tells the walk engine which walk to use
-                PROPERTY(bool, chest_pressed_long)      ///< chest pressed for more than half a second
-                PROPERTY(bool, left_foot_pressed_long)  ///< left foot pressed for more than half a second
-                PROPERTY(bool, right_foot_pressed_long)  ///< right foot pressed for more than half a second
-                PROPERTY(bool, chest_pressed)      ///< chest pressed 
-                PROPERTY(bool, left_foot_pressed)  ///< left foot pressed 
-                PROPERTY(bool, right_foot_pressed)  ///< right foot pressed 
-                int16_t pad;
         public:
-                NAO_State(): _stance(Standing), _walk(Modded_UNSW_Walk),_chest_pressed_long(false), _left_foot_pressed_long(false), _right_foot_pressed_long(false), _chest_pressed(0), _left_foot_pressed(0), _right_foot_pressed(0) {}
+                NAO_State(): wb_nao_state() {}
 
                 bool fallen() const { return stance() == FallenForward || stance() == FallenBack || stance() == FallenLeft || stance() == FallenRight; }
 
 #ifdef WHITEBOARD_POSTER_STRING_CONVERSION
                 /** string constructor */
-                NAO_State(const std::string &command): _stance(Standing), _walk(Modded_UNSW_Walk), _chest_pressed_long(false), _left_foot_pressed_long(false), _right_foot_pressed_long(false), _chest_pressed(0), _left_foot_pressed(0), _right_foot_pressed(0) { from_string(command); }
+                NAO_State(const std::string &command): wb_nao_state() { from_string(command); }
 
                 /** convert to a string */
                 std::string description()
