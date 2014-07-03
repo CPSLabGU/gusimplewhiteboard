a0404f9fae99ae4a7e5250f20f906fcbfb39a37d
Eugene Gilmore <eugene.gilmore@griffithuni.edu.au>
2014-07-03 13:49:16 +1000

    fix up visiongoals wb message

---
+++ typeClassDefs/wb_goal.h
@@ -0,0 +1,20 @@
+#ifndef _WBGOAL_
+#define _WBGOAL_
+
+#include <sys/types.h>
+#include <gu_util.h>
+#include <SimpleShapes.h>
+
+struct wb_goal
+{
+	PROPERTY(GUPoint<int16_t>, outerBottom)
+	PROPERTY(GUPoint<int16_t>, outerTop)
+	PROPERTY(GUPoint<int16_t>, innerBottom)
+	PROPERTY(GUPoint<int16_t>, innerTop)
+        PROPERTY(bool, visible)
+#ifdef __cplusplus
+	wb_goal() : _outerBottom(), _outerTop(), _innerBottom(), _innerTop(), _visible(false) {}
+#endif
+};
+
+#endif
