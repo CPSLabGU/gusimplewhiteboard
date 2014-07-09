18bf77a973cfc01bce0d4f05214e5b32418d0fa8
Carl Lusty <carl.lusty@griffithuni.edu.au>
2014-07-09 13:57:30 +1000

    Changed default resolution on the horiz angle function to 640

--- typeClassDefs/FilteredOneDimObject.h
+++ typeClassDefs/FilteredOneDimObject.h
@@ -136,7 +136,7 @@ public:
     }
     
         /********************* Useful utilities *****************************/
-        float horizontal_angle(const float guvision_width = 1280.0f, const float horiz_fov = 61.0f) const
+        float horizontal_angle(const float guvision_width = 640.0f, const float horiz_fov = 61.0f) const
         {
                 float yaw_in_radians = float(DEG2RAD(yaw()));                       // head yaw in radians
                 float D = guvision_width / 2 / sinf(float(DEG2RAD(horiz_fov/2)));   // projection distance in pixel units
