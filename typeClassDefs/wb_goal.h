9552816acfb636a62f69eb8a4b6817e1d8b6d8cd
Eugene Gilmore <eugene.gilmore@griffithuni.edu.au>
2014-07-18 12:38:17 +1000

    fix vision warning

--- typeClassDefs/wb_goal.h
+++ typeClassDefs/wb_goal.h
@@ -14,7 +14,7 @@ struct wb_goal
         PROPERTY(bool, visible)
         PROPERTY(bool, pad)                         ///< align to 16 bits
 #ifdef __cplusplus
-	wb_goal() : _outerBottom(), _outerTop(), _innerBottom(), _innerTop(), _visible(false) {}
+	wb_goal() : _outerBottom(), _outerTop(), _innerBottom(), _innerTop(), _visible(false), _pad(false) {}
 #endif
 };
 
