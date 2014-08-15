829160cc079f6eb9d7097488b504f3943b8754b4
Carl Lusty <carl.lusty@griffithuni.edu.au>
2014-08-15 13:46:26 +1000

    Updating docs

--- guudpwhiteboard/udp_receiver.h
+++ guudpwhiteboard/udp_receiver.h
@@ -56,14 +56,35 @@ public:
 
         gu_simple_whiteboard_descriptor **remote_wbd;
         int _machines_in_the_network;
-        
+
+	/**
+ 	* Setup the receiver object 
+ 	*/       
         Receiver(gsw_udp_packet_info *packet_data, int packets_in_schedule, int max_types_per_packet, int machines_in_the_network);
+
+	/**
+ 	* make storage object
+ 	*/       
         void construct_packets_array();
+
+	/**
+ 	* Set the socket to non-blocking
+ 	*/       
         void set_nonblock(int socket);
-        // get sockaddr, IPv4 or IPv6:
+
+	/**
+ 	* get sockaddr, IPv4 or IPv6
+ 	*/       
         void *get_in_addr(struct sockaddr_in *sa);
+
+	/**
+ 	* fetch packet
+ 	*/       
         void get_packet(int max_packet_size);
 
+	/**
+ 	* delete objects
+ 	*/       
         ~Receiver();
 };
 
