906eb26c3ef6ff4ff9500e286d7bc83f8e8b7502
Carl Lusty <carl.lusty@griffithuni.edu.au>
2014-08-25 15:30:20 +1000

    doxy

--- typeClassDefs/wb_nao_state.h
+++ typeClassDefs/wb_nao_state.h
@@ -34,17 +34,27 @@ enum Robot_Walk{
  */
 struct wb_nao_state
 {
-	PROPERTY(Robot_Stance, stance)          ///< Currently SMRobotPosition detects if the robot has fallen over. If not fallen over SMRobotPosition posts Standing.
-	PROPERTY(Robot_Walk, walk)          ///< Tells the walk engine which walk to use
-	PROPERTY(bool, chest_pressed_long)      ///< chest pressed for more than half a second
-	PROPERTY(bool, left_foot_pressed_long)  ///< left foot pressed for more than half a second
-	PROPERTY(bool, right_foot_pressed_long)  ///< right foot pressed for more than half a second
-	PROPERTY(bool, chest_pressed)      ///< chest pressed 
-	PROPERTY(bool, left_foot_pressed)  ///< left foot pressed 
-	PROPERTY(bool, right_foot_pressed)  ///< right foot pressed 
+	/** Currently SMRobotPosition detects if the robot has fallen over. If not fallen over SMRobotPosition posts Standing. */
+	PROPERTY(Robot_Stance, stance)          
+	/** Tells the walk engine which walk to use */
+	PROPERTY(Robot_Walk, walk)          
+	/** chest pressed for more than half a second */
+	PROPERTY(bool, chest_pressed_long)      
+	/** left foot pressed for more than half a second */
+	PROPERTY(bool, left_foot_pressed_long)  
+	/** right foot pressed for more than half a second */
+	PROPERTY(bool, right_foot_pressed_long)  
+	/** chest pressed  */
+	PROPERTY(bool, chest_pressed)      
+	/** left foot pressed  */
+	PROPERTY(bool, left_foot_pressed)  
+	/** right foot pressed  */
+	PROPERTY(bool, right_foot_pressed)  
+	/** padding */
 	int16_t pad;
 
 #ifdef __cplusplus
+	/** constructor */
 	wb_nao_state(): _stance(Standing), _walk(Modded_UNSW_Walk),_chest_pressed_long(false), _left_foot_pressed_long(false), _right_foot_pressed_long(false), _chest_pressed(0), _left_foot_pressed(0), _right_foot_pressed(0) {}
 #endif
 };
