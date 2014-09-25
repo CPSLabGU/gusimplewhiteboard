d617d3b11e3386ae8213d4dd746c767c8eb0de92
Rene Hexel <r.hexel@griffith.edu.au>
2014-09-26 08:38:45 +1000

    Back out DifferentialRobotControlStatus.h as it's not been pushed

--- guwhiteboardposter.h
+++ guwhiteboardposter.h
@@ -89,7 +89,7 @@
 #include "VisionBall.h"
 #include "VisionGoals.h"
 #include "WalkData.h"
-#include "DifferentialRobotControlStatus.h"
+//#include "DifferentialRobotControlStatus.h"   // needs to be committed first!
 #endif // WHITEBOARD_POSTER_STRING_CONVERSION
 
 extern "C"
