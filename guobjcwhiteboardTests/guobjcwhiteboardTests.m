cf5899947097c28615c6cd2e7bccb98d2976db80
Rene Hexel <r.hexel@griffith.edu.au>
2014-10-20 09:19:33 +1000

    Add simple tests for OC whiteboard

--- guobjcwhiteboardTests/guobjcwhiteboardTests.m
+++ guobjcwhiteboardTests/guobjcwhiteboardTests.m
@@ -3,30 +3,48 @@
 //  guobjcwhiteboardTests
 //
 //  Created by René Hexel on 6/05/12.
-//  Copyright (c) 2012 Rene Hexel. All rights reserved.
+//  Copyright (c) 2012, 2014 Rene Hexel. All rights reserved.
 //
+#import <XCTest/XCTest.h>
+#import <gusimplewhiteboard.h>
+#import "ObjCWhiteboard.h"
+#import <guwhiteboardtypelist_c_generated.h>
 
-#import "guobjcwhiteboardTests.h"
+@interface guobjcwhiteboardTests: XCTestCase
+
+@property (nonatomic, strong) ObjCWhiteboard *whiteboard;
+
+@end
+
 
 @implementation guobjcwhiteboardTests
 
 - (void)setUp
 {
     [super setUp];
-    
-    // Set-up code here.
+
+    self.whiteboard = [[ObjCWhiteboard alloc] init];
 }
 
 - (void)tearDown
 {
-    // Tear-down code here.
-    
+    self.whiteboard = nil;
+
     [super tearDown];
 }
 
-- (void)testExample
+- (void) testPost
 {
-    STFail(@"Unit tests are not implemented yet in guobjcwhiteboardTests");
+    XCTAssertTrue([self.whiteboard postWBMessageOfType: kSpeech_v withContent: @"Hello"]);
+}
+
+- (void) testGet
+{
+    NSString *testString = NSStringFromSelector(_cmd);
+    XCTAssertTrue([self.whiteboard postWBMessageOfType: kSpeech_v withContent: testString]);
+    NSString *result = [self.whiteboard getMessageOfType: kSpeech_v];
+    XCTAssertNotNil(result, @"Expected non-nil result for speech message");
+    XCTAssertEqualObjects(result, testString, @"Expected wb get to yield post result");
 }
 
 @end
