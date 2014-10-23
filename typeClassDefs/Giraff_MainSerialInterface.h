5ec7b85f72ab055169152b934fe65b32bdb9856b
carl <powidl+giraff@me.com>
2014-10-23 16:35:49 +1000

    Now posting status messages

--- typeClassDefs/Giraff_MainSerialInterface.h
+++ typeClassDefs/Giraff_MainSerialInterface.h
@@ -11,6 +11,11 @@
 
 #include "wb_giraff_main_serial.h" 
 
+#ifdef WHITEBOARD_POSTER_STRING_CONVERSION
+#include <arpa/inet.h>
+#include <sstream>
+#endif
+
 namespace guWhiteboard                                                  
 {
         /**
@@ -36,23 +41,43 @@ namespace guWhiteboard
         class Giraff_MainSerialInterface: public wb_giraff_main_serial
         {
         public:
+
+        /** default constructor 
+		 */
+		Giraff_MainSerialInterface() { }
+
 #ifdef WHITEBOARD_POSTER_STRING_CONVERSION
-                /** string constructor (see from_string() below) - NYI
+                /** string constructor (see from_string() below)
 		 *  @param[in] str a serialised string containing properties to set in this class
 		 */
 		Giraff_MainSerialInterface(const std::string &str) { from_string(str); }
 
-                /** parse class properties from a string - NYI
+                /** parse class properties from a string
 		 *  @param[in] str a serialised string containing properties to set in this class
 		 */
 #define DELIMITER ','
 #define SEP ':'
 #define PARSER(s, c, p) if (k.compare(s) == 0) { set_##c ( p ); }
 #define SV std::vector<std::string>
-#define PARSE_FLOAT static_cast<float>(atof(v.c_str()))
-#define PARSE_INT16 static_cast<int16_t>(atoi(v.c_str()))
-#define PARSE_INT32 static_cast<int32_t>(atoi(v.c_str()))
-#define PARSE_BITSET static_cast<uint8_t>(0) //NYI
+#define COMP(v, s) v.compare(0, 2, s) == 0
+#define IS_HEX(v) COMP(v, "F*") || COMP(v, "I*")
+#define READ_HEX(v) ^(void){ \
+        uint32_t num;                           \
+        char *ch = (char *)v.substr(2).c_str(); \
+        sscanf(ch, "%x", &num);                 \
+        int temp =  htonl(*(unsigned int*)&num);\
+        return temp; }()
+
+#define PARSE_HEX_FLOAT(v) ^(void) { int tmp = READ_HEX(v); \
+                                     return *((float*)&tmp); }()
+#define PARSE_HEX_INT16(v) ^(void) { return static_cast<int16_t>(READ_HEX(v)); }()
+#define PARSE_HEX_INT32(v) ^(void) { return static_cast<int32_t>(READ_HEX(v)); }()
+#define PARSE_HEX_BITSET(v) ^(void) { return static_cast<int8_t>(0); }()
+
+#define PARSE_FLOAT IS_HEX(v) ? PARSE_HEX_FLOAT(v) : static_cast<float>(atof(v.c_str()))
+#define PARSE_INT16 IS_HEX(v) ? PARSE_HEX_INT16(v) : static_cast<int16_t>(atoi(v.c_str()))
+#define PARSE_INT32 IS_HEX(v) ? PARSE_HEX_INT32(v) : static_cast<int32_t>(atoi(v.c_str()))
+#define PARSE_BITSET IS_HEX(v) ? PARSE_HEX_BITSET(v) : static_cast<uint8_t>(0) //NYI
                 void from_string(const std::string &str) 
 		{
 			SV sn = components_of_string_separated(str, DELIMITER, true);
@@ -81,8 +106,8 @@ namespace guWhiteboard
 				PARSER("vgr", vgr, PARSE_FLOAT)
 				PARSER("cdp", cdp, PARSE_FLOAT)
 				PARSER("cvg", cvg, PARSE_FLOAT)
-				//PARSER("but0", but0, PARSE_INT16)
-				//PARSER("but1", but1, PARSE_INT16)
+				PARSER("but0", but0, PARSE_INT16)
+				PARSER("but1", but1, PARSE_INT16)
 				PARSER("dial", dial, PARSE_INT16)
 				PARSER("enc0", enc0, PARSE_INT32)
 				PARSER("enc1", enc1, PARSE_INT32)
@@ -99,7 +124,7 @@ namespace guWhiteboard
                 std::string description() const
                 {
                         std::stringstream ss;
-			ss << v() << " : v, " << r() << " : r, " << a() << " : a, " << p() << " : p, " << /*mode() <<*/ " : mode, " << /*tilt_homing_state() <<*/ " : tilt_homing_state, " << tilt_angle_from_home() << " : tilt_angle_from_home, " << vg() << " : vg, " << vgr() << " : vgr, " << cdp() << " : cdp, " << cvg() << " : cvg, " << but0() << " : but0, " << but1() << " : but1, " << dial() << " : dial, " << enc0() << " : enc0, " << enc1() << " : enc1, " << enc2() << " : enc2";
+                        ss << v() << " : v, " << r() << " : r, " << a() << " : a, " << p() << " : p, " << /*mode() <<*/ " : mode, " << /*tilt_homing_state() <<*/ " : tilt_homing_state, " << tilt_angle_from_home() << " : tilt_angle_from_home, " << vg() << " : vg, " << vgr() << " : vgr, " << cdp() << " : cdp, " << cvg() << " : cvg, " << but0() << " : but0, " << but1() << " : but1, " << dial() << " : dial, " << enc0() << " : enc0, " << enc1() << " : enc1, " << enc2() << " : enc2";
                         return ss.str();
                 }
 #endif // WHITEBOARD_POSTER_STRING_CONVERSION
