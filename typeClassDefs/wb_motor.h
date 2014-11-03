8888060a1d609c4e3d5762880ebe48b934c5a38d
Carl <nil>
2014-11-03 15:25:37 +1000

    Doxy

--- typeClassDefs/wb_motor.h
+++ typeClassDefs/wb_motor.h
@@ -4,11 +4,16 @@
 #include <sys/types.h>
 #include <gu_util.h>
 
+/** Basic motor struct */
 struct wb_motor {
-	PROPERTY(int8_t, speed)  //Speed setting -127 to 127 in cm/s
-	PROPERTY(uint8_t, accel) //Acceleration setting 0 to 255 in cm/s
-	PROPERTY(uint16_t, dist) //Odometer used for storing distance travelled
+	/** Speed setting -127 to 127 in cm/s */
+	PROPERTY(int8_t, speed)  
+	/** Acceleration setting 0 to 255 in cm/s */
+	PROPERTY(uint8_t, accel) 
+	/** Odometer used for storing distance travelled */
+	PROPERTY(uint16_t, dist) 
 #ifdef __cplusplus
+	/** Default constructor */
 	wb_motor() : _speed(100), _accel(127), _dist(0) {}
 #endif
 };
