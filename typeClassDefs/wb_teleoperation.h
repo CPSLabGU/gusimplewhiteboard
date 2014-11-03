3aaf3c5061fbf4045e22fb16a5fa2cb510ab50b6
Carl <nil>
2014-11-03 15:23:46 +1000

    Doxy

--- typeClassDefs/wb_teleoperation.h
+++ typeClassDefs/wb_teleoperation.h
@@ -6,15 +6,21 @@
 #include "MOTION_Interface.h"
 #include "Vision_Control.h"
 
+/** struct for the teleoperation class */
 struct wb_teleoperation
 {
-	unsigned initBit : 1;
+	unsigned initBit : 1; ///< Gross hacky way of detecting a connection
+	/** The forward speed */
 	PROPERTY(int, fowardWalkSpeed)
+	/** Turning speed */
 	PROPERTY(float, turnWalkSpeed)
+	/** Head pitch controller */
 	PROPERTY(float, headPitch)
+	/** Head yaw controller */
 	PROPERTY(float, headYaw)
 
 #ifdef __cplusplus
+	/** default constructor */
 	wb_teleoperation() : initBit(0), _fowardWalkSpeed(0), _turnWalkSpeed(0), _headPitch(0), _headYaw(0){}
 #endif
 };
