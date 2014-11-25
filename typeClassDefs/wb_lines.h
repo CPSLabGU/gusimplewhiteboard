541e4ebfe7dbf8261142c5b17499424d308a6cff
Eugene Gilmore <eugene.gilmore@griffithuni.edu.au>
2014-11-25 17:17:48 +1000

    fix docs typo

--- typeClassDefs/wb_lines.h
+++ typeClassDefs/wb_lines.h
@@ -35,12 +35,12 @@ struct wb_lines {
 	/**Array of lines found in the top camera in this frame*/
 	ARRAY_PROPERTY(wb_line, bottomLines, 6)
 	/**
-	 * @briefMask for lines seen in top camera
+	 * @brief Mask for lines seen in top camera
 	 * first bit represent visibility of first line in the array. Last two bits unused
 	 */
 	PROPERTY(int8_t, topMask)
 	/**
-	 * @briefMask for lines seen in bottom camera
+	 * @brief Mask for lines seen in bottom camera
 	 * first bit represent visibility of first line in the array. Last two bits unused
 	 */
 	PROPERTY(int8_t, bottomMask)
