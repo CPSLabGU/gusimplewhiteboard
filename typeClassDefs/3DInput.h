968951ace4a90059965e0400eca26044b2095a5d
Carl Lusty <carl.lusty@griffithuni.edu.au>
2015-07-31 19:28:33 +1000

    Due to the macro not allowing 3DInput as a name I have changed to Input3D

--- typeClassDefs/3DInput.h
+++ typeClassDefs/3DInput.h
@@ -2,8 +2,8 @@
 /* is <class_name>_DEFINED              */
 /***************************************/
 
-#ifndef 3DInput_DEFINED
-#define 3DInput_DEFINED
+#ifndef Input3D_DEFINED
+#define Input3D_DEFINED
 
 #ifdef WHITEBOARD_POSTER_STRING_CONVERSION
 #include <cstdlib>
@@ -17,26 +17,26 @@ namespace guWhiteboard
         /**
          * Class for for demonstrating OO-messages.
          */
-        class 3DInput: public wb_3dinput
+        class Input3D: public wb_3dinput
         {
         public:
             /** designated constructor */
-            3DInput(int16_t yaw = 0, int16_t pitch = 0, int16_t roll = 0, int16_t power = 0): wb_3dinput(yaw, pitch, roll, power) { }
+            Input3D(int16_t yaw = 0, int16_t pitch = 0, int16_t roll = 0, int16_t power = 0): wb_3dinput(yaw, pitch, roll, power) { }
 
             /** copy constructor */
-            3DInput(const 3DInput &other): wb_3dinput(other.yaw(), other.pitch(), other.roll(), other.power()) {}
+            Input3D(const Input3D &other): wb_3dinput(other.yaw(), other.pitch(), other.roll(), other.power()) {}
 
             /** copy assignment operator */
-            3DInput &operator=(const 3DInput &other) { set_yaw(other.yaw()); set_pitch(other.pitch()); set_roll(other.roll()); set_power(other.power()); return *this; }
+            Input3D &operator=(const Input3D &other) { set_yaw(other.yaw()); set_pitch(other.pitch()); set_roll(other.roll()); set_power(other.power()); return *this; }
 
 #ifdef WHITEBOARD_POSTER_STRING_CONVERSION
             /** string constructor */
-            3DInput(const std::string &names) { from_string(names); }
+            Input3D(const std::string &names) { from_string(names); }
 
             /** const char *constructor */
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wc++98-compat"
-            3DInput(const char *names): 3DInput(std::string(names)) { }
+            Input3D(const char *names): Input3D(std::string(names)) { }
 #pragma clang diagnostic pop
             /** convert to a string */
             std::string description()
@@ -54,4 +54,4 @@ namespace guWhiteboard
 #endif // WHITEBOARD_POSTER_STRING_CONVERSION
         };
 }
-#endif // 3DInput_DEFINED
+#endif // Input3D_DEFINED
