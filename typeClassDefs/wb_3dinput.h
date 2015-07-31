8df5f554be86109a488e5df9340bb0683ddb40c0
Carl Lusty <carl.lusty@griffithuni.edu.au>
2015-07-31 19:41:50 +1000

    Leap input abstraction compiling (WIP)

--- typeClassDefs/wb_3dinput.h
+++ typeClassDefs/wb_3dinput.h
@@ -1,7 +1,7 @@
-#ifndef 3DInput_h
-#define 3DInput_h
+#ifndef wb_3dinput_h
+#define wb_3dinput_h
 
-#include <wb_point.h>
+#include <gu_util.h>
 
 /**
  * Simple 3 dimensional input class
@@ -16,6 +16,11 @@ struct wb_3dinput
     PROPERTY(int16_t, roll)
     /** power value, ie. high of leap hand or thrust of flight simulator */
     PROPERTY(int16_t, power)
+
+#ifdef __cplusplus
+    /** constructor */
+    wb_3dinput(int16_t yaw = 0, int16_t pitch = 0, int16_t roll = 0, int16_t power = 0): _yaw(yaw), _pitch(pitch), _roll(roll), _power(power) {}
+#endif
 };
 
-#endif //3DInput_h
+#endif //wb_3dinput_h
