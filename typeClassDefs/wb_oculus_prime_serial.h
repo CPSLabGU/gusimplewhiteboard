d5679e21ff51b353a1ba36f69a469d9a008b4219
Carl Lusty <carl.lusty@griffithuni.edu.au>
2015-08-04 13:37:16 +1000

    Start of WIP prime interface

---
+++ typeClassDefs/wb_oculus_prime_serial.h
@@ -0,0 +1,50 @@
+/**                                                                     
+ *  /file wb_oculus_prime_serial.h 
+ *                                                                      
+ *  Created by Carl Lusty in 2015.                                      
+ *  Copyright (c) 2015 Carl Lusty                                       
+ *  All rights reserved.                                                
+ */                                                                     
+ 
+#ifndef wb_oculus_prime_serial_h
+#define wb_oculus_prime_serial_h
+
+#include <gu_util.h>
+
+struct wb_byte_pair
+{
+    /** first byte */
+    PROPERTY(uint8_t, first)
+    /** second byte */
+    PROPERTY(uint8_t, second)
+};
+
+/**
+ * Serial interface for the main giraff board.
+ * This includes the buttons, battery data and motor control.
+ */
+struct wb_oculus_prime_serial
+{
+    /** speed for each wheel */
+    CONTROLLED_PROPERTY(wb_byte_pair, forward)
+
+
+
+    /** stop */
+    BIT_PROPERTY(stop)
+
+    /** Control bit (mask) */
+    CONTROL_BIT(forward)
+
+#ifdef __cplusplus
+    /**
+    * stuct constructor 
+    */       
+    wb_oculus_prime_serial()
+    {
+        memset(this, 0, sizeof(*this));
+    }
+#endif
+};
+
+#endif //_wb_oculus_prime_serial_h
