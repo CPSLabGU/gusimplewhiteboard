57031aa0b2e26916d205f1032b4caecd0cf2062c
Carl Lusty <carl.lusty@griffithuni.edu.au>
2016-03-08 20:07:35 +1000

    Adding IoT control message. Basically a mini wb IoT nodes (personal)

--- guwhiteboardposter.cpp
+++ guwhiteboardposter.cpp
@@ -1049,6 +1049,18 @@ bool guWhiteboard::postmsg(WBTypes message_index, std::string message_content)
 			return true;
 		}
 
+		case kIoT_Control_v:
+#ifdef IoT_Control_DEFINED
+		{
+/** WB Ptr Class: IoT_Control @brief Nil */ 
+			class IoT_Control_t IoT_Control_msg;
+			IoT_Control_msg.post(IoT_Control(message_content));
+			return true;
+		}
+#else
+			return false;
+#endif // !IoT_Control_DEFINED
+
 	}
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunreachable-code"
@@ -1060,7 +1072,7 @@ bool guWhiteboard::postmsg(WBTypes message_index, std::string message_content)
 whiteboard_types_map::whiteboard_types_map(): map<string, WBTypes>()
 {
 	whiteboard_types_map &self = *this;
-	// self.reserve(93);
+	// self.reserve(94);
 
 	self["*"] = kwb_reserved_SubscribeToAllTypes_v;
 	self["Print"] = kPrint_v;
@@ -1155,5 +1167,6 @@ whiteboard_types_map::whiteboard_types_map(): map<string, WBTypes>()
 	self["TurnRedNS"] = kTurnRedNS_v;
 	self["RedNSon"] = kRedNSon_v;
 	self["TimerReset"] = kTimerReset_v;
+	self["IoT_Control"] = kIoT_Control_v;
 }
 
