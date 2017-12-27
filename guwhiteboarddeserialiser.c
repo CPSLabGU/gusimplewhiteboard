faaaec39c8dfb545701922aaae64e3f4167bc551
Carl Lusty <carl.lusty@griffithuni.edu.au>
2017-12-27 19:25:32 +1000

    regen

--- guwhiteboarddeserialiser.c
+++ guwhiteboarddeserialiser.c
@@ -10,236 +10,462 @@ bool deserialisemsg(WBTypes message_index, void *serialised_in, void *message_ou
 {
     switch (message_index)
     {
-		case kwb_reserved_SubscribeToAllTypes_v:
+		case kwb_reserved_SubscribeToAllTypes_v: {
 			return false;
-		case kPrint_v:
+			break;
+		}
+		case kPrint_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kSay_v:
+			break;
+		}
+		case kSay_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kSpeech_v:
+			break;
+		}
+		case kSpeech_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kQSay_v:
+			break;
+		}
+		case kQSay_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kQSpeech_v:
+			break;
+		}
+		case kQSpeech_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kSpeechOutput_v:
+			break;
+		}
+		case kSpeechOutput_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kGCGameState_v:
+			break;
+		}
+		case kGCGameState_v: {
 			return false;
-		case kREMOVED1_v:
+			break;
+		}
+		case kREMOVED1_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kREMOVED2_v:
+			break;
+		}
+		case kREMOVED2_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kMOTION_Commands_v:
+			break;
+		}
+		case kMOTION_Commands_v: {
 			return false;
-		case kMOTION_Status_v:
+			break;
+		}
+		case kMOTION_Status_v: {
 			return false;
-		case kHAL_HeadTarget_v:
+			break;
+		}
+		case kHAL_HeadTarget_v: {
 			return false;
-		case kSENSORSFootSensors_v:
+			break;
+		}
+		case kSENSORSFootSensors_v: {
 			return false;
-		case kSENSORSBodySensors_v:
+			break;
+		}
+		case kSENSORSBodySensors_v: {
 			return false;
-		case kSENSORSLedsSensors_v:
+			break;
+		}
+		case kSENSORSLedsSensors_v: {
 			return false;
-		case kSENSORSLegJointTemps_v:
+			break;
+		}
+		case kSENSORSLegJointTemps_v: {
 			return false;
-		case kSENSORSTorsoJointTemps_v:
+			break;
+		}
+		case kSENSORSTorsoJointTemps_v: {
 			return false;
-		case kSENSORSLegJointSensors_v:
+			break;
+		}
+		case kSENSORSLegJointSensors_v: {
 			return false;
-		case kSENSORSTorsoJointSensors_v:
+			break;
+		}
+		case kSENSORSTorsoJointSensors_v: {
 			return false;
-		case kSENSORSSonarSensors_v:
+			break;
+		}
+		case kSENSORSSonarSensors_v: {
 			return false;
-		case kFSM_Control_v:
+			break;
+		}
+		case kFSM_Control_v: {
 			return false;
-		case kFSM_Status_v:
+			break;
+		}
+		case kFSM_Status_v: {
 			return false;
-		case kFSM_Names_v:
+			break;
+		}
+		case kFSM_Names_v: {
 			return false;
-		case kSoloTypeExample_v:
+			break;
+		}
+		case kSoloTypeExample_v: {
 			return false;
-		case kFilteredGoalSighting_v:
+			break;
+		}
+		case kFilteredGoalSighting_v: {
 			return false;
-		case kNAO_State_v:
+			break;
+		}
+		case kNAO_State_v: {
 			return false;
-		case kUDPRN_v:
+			break;
+		}
+		case kUDPRN_v: {
 			return false;
-		case kPlayerNumber_v:
+			break;
+		}
+		case kPlayerNumber_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kManuallyPenalized_v:
+			break;
+		}
+		case kManuallyPenalized_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kVision_Control_v:
+			break;
+		}
+		case kVision_Control_v: {
 			return false;
-		case kVision_Status_v:
+			break;
+		}
+		case kVision_Status_v: {
 			return false;
-		case kFFTStatus_v:
+			break;
+		}
+		case kFFTStatus_v: {
 			return false;
-		case kFSOsighting_v:
+			break;
+		}
+		case kFSOsighting_v: {
 			return false;
-		case kTopParticles_v:
+			break;
+		}
+		case kTopParticles_v: {
 			return false;
-		case kFilteredBallSighting_v:
+			break;
+		}
+		case kFilteredBallSighting_v: {
 			return false;
-		case kPF_ControlStatus_Modes_v:
+			break;
+		}
+		case kPF_ControlStatus_Modes_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kWEBOTS_NXT_bridge_v:
+			break;
+		}
+		case kWEBOTS_NXT_bridge_v: {
 			return false;
-		case kWEBOTS_NXT_encoders_v:
+			break;
+		}
+		case kWEBOTS_NXT_encoders_v: {
 			return false;
-		case kWEBOTS_NXT_camera_v:
+			break;
+		}
+		case kWEBOTS_NXT_camera_v: {
 			return false;
-		case kWEBOTS_NXT_walk_isRunning_v:
+			break;
+		}
+		case kWEBOTS_NXT_walk_isRunning_v: {
 			return false;
-		case kWEBOTS_NXT_deadReakoning_walk_v:
+			break;
+		}
+		case kWEBOTS_NXT_deadReakoning_walk_v: {
 			return false;
-		case kWEBOTS_NXT_colorLine_walk_v:
+			break;
+		}
+		case kWEBOTS_NXT_colorLine_walk_v: {
 			return false;
-		case kWEBOTS_NXT_gridMotions_v:
+			break;
+		}
+		case kWEBOTS_NXT_gridMotions_v: {
 			return false;
-		case kVisionBall_v:
+			break;
+		}
+		case kVisionBall_v: {
 			return false;
-		case kVisionGoals_v:
+			break;
+		}
+		case kVisionGoals_v: {
 			return false;
-		case kWalkData_v:
+			break;
+		}
+		case kWalkData_v: {
 			return false;
-		case kTeleoperationControlStatus_v:
+			break;
+		}
+		case kTeleoperationControlStatus_v: {
 			return false;
-		case kTeleoperationConnection_v:
+			break;
+		}
+		case kTeleoperationConnection_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kUDPWBNumber_v:
+			break;
+		}
+		case kUDPWBNumber_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kWEBOTS_NXT_bumper_v:
+			break;
+		}
+		case kWEBOTS_NXT_bumper_v: {
 			return false;
-		case kWEBOTS_NXT_vector_bridge_v:
+			break;
+		}
+		case kWEBOTS_NXT_vector_bridge_v: {
 			return false;
-		case kVisionLines_v:
+			break;
+		}
+		case kVisionLines_v: {
 			return false;
-		case kDifferentialRobotStatus_v:
+			break;
+		}
+		case kDifferentialRobotStatus_v: {
 			return false;
-		case kDifferentialRobotControl_v:
+			break;
+		}
+		case kDifferentialRobotControl_v: {
 			return false;
-		case kXEyesPos_v:
+			break;
+		}
+		case kXEyesPos_v: {
 			return false;
-		case kVisionFace_v:
+			break;
+		}
+		case kVisionFace_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kDraw_v:
+			break;
+		}
+		case kDraw_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kFSM_States_v:
+			break;
+		}
+		case kFSM_States_v: {
 			return false;
-		case kGiraff_Interface_Status_v:
+			break;
+		}
+		case kGiraff_Interface_Status_v: {
 			return false;
-		case kGiraff_Interface_Command_v:
+			break;
+		}
+		case kGiraff_Interface_Command_v: {
 			return false;
-		case kNXT_Status_v:
+			break;
+		}
+		case kNXT_Status_v: {
 			return false;
-		case kNXT_Command_v:
+			break;
+		}
+		case kNXT_Command_v: {
 			return false;
-		case kAPM_Status_v:
+			break;
+		}
+		case kAPM_Status_v: {
 			return false;
-		case kAPM_Command_v:
+			break;
+		}
+		case kAPM_Command_v: {
 			return false;
-		case kREMOVED3_v:
+			break;
+		}
+		case kREMOVED3_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kREMOVED4_v:
+			break;
+		}
+		case kREMOVED4_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kCBall_v:
+			break;
+		}
+		case kCBall_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kOculusPrime_Command_v:
+			break;
+		}
+		case kOculusPrime_Command_v: {
 			return false;
-		case kInput3D_v:
+			break;
+		}
+		case kInput3D_v: {
 			return false;
-		case kOculus_Prime_Command_v:
+			break;
+		}
+		case kOculus_Prime_Command_v: {
 			return false;
-		case kIOPins_v:
+			break;
+		}
+		case kIOPins_v: {
 			return false;
-		case kNXT_Two_Touch_Status_v:
+			break;
+		}
+		case kNXT_Two_Touch_Status_v: {
 			return false;
-		case kNXT_Sound_Control_v:
+			break;
+		}
+		case kNXT_Sound_Control_v: {
 			return false;
-		case kNXT_Lights_Control_v:
+			break;
+		}
+		case kNXT_Lights_Control_v: {
 			return false;
-		case kClocks_v:
+			break;
+		}
+		case kClocks_v: {
 			return false;
-		case kChannels_v:
+			break;
+		}
+		case kChannels_v: {
 			return false;
-		case kSwitchSubsumption_v:
+			break;
+		}
+		case kSwitchSubsumption_v: {
 			return false;
-		case kTotoDoingMotion_v:
+			break;
+		}
+		case kTotoDoingMotion_v: {
 			return false;
-		case kCount_v:
+			break;
+		}
+		case kCount_v: {
 			return false;
-		case kGreenEWon_v:
+			break;
+		}
+		case kGreenEWon_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kWarnEW_v:
+			break;
+		}
+		case kWarnEW_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kTimeGTthirty_v:
+			break;
+		}
+		case kTimeGTthirty_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kAmberEWon_v:
+			break;
+		}
+		case kAmberEWon_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kTurnRedEW_v:
+			break;
+		}
+		case kTurnRedEW_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kTimeGTfive_v:
+			break;
+		}
+		case kTimeGTfive_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kRedEWon_v:
+			break;
+		}
+		case kRedEWon_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kGreenNSon_v:
+			break;
+		}
+		case kGreenNSon_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kWarnNS_v:
+			break;
+		}
+		case kWarnNS_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kAmberNSon_v:
+			break;
+		}
+		case kAmberNSon_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kTurnRedNS_v:
+			break;
+		}
+		case kTurnRedNS_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kRedNSon_v:
+			break;
+		}
+		case kRedNSon_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kTimerReset_v:
+			break;
+		}
+		case kTimerReset_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kSLOT_UNUSED_v:
+			break;
+		}
+		case kSLOT_UNUSED_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kCarSensorPressed_v:
+			break;
+		}
+		case kCarSensorPressed_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kSwitchSubsumptionTrafficLights_v:
+			break;
+		}
+		case kSwitchSubsumptionTrafficLights_v: {
 			return false;
-		case kBall_Found_v:
+			break;
+		}
+		case kBall_Found_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kBall_Calibration_File_v:
+			break;
+		}
+		case kBall_Calibration_File_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kBall_Calibration_Num_v:
+			break;
+		}
+		case kBall_Calibration_Num_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kBall_Color_Num_v:
+			break;
+		}
+		case kBall_Color_Num_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kHAL_LArmTarget_Ctrl_v:
+			break;
+		}
+		case kHAL_LArmTarget_Ctrl_v: {
 			return false;
-		case kHAL_LArmTarget_Stat_v:
+			break;
+		}
+		case kHAL_LArmTarget_Stat_v: {
 			return false;
-		case kHAL_LArmTarget_Tolr_v:
+			break;
+		}
+		case kHAL_LArmTarget_Tolr_v: {
 			return false;
-		case kHAL_RArmTarget_Ctrl_v:
+			break;
+		}
+		case kHAL_RArmTarget_Ctrl_v: {
 			return false;
-		case kHAL_RArmTarget_Stat_v:
+			break;
+		}
+		case kHAL_RArmTarget_Stat_v: {
 			return false;
-		case kHAL_RArmTarget_Tolr_v:
+			break;
+		}
+		case kHAL_RArmTarget_Tolr_v: {
 			return false;
-		case kVisionFieldFeatures_v:
+			break;
+		}
+		case kVisionFieldFeatures_v: {
 			return false;
-		case kWhistleBlown_v:
+			break;
+		}
+		case kWhistleBlown_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kVolumeControl_v:
+			break;
+		}
+		case kVolumeControl_v: {
 			return false; /*TODO, add support for POD types.*/
-		case kVisionRobots_v:
+			break;
+		}
+		case kVisionRobots_v: {
 			return false;
-		case kFieldHorizon_v:
+			break;
+		}
+		case kFieldHorizon_v: {
 			return false;
-		case kNaoWalkCommand_v:
+			break;
+		}
+		case kNaoWalkCommand_v: {
 			return false;
-		case kNaoWalkStatus_v:
+			break;
+		}
+		case kNaoWalkStatus_v: {
 			return false;
+			break;
+		}
 	}
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wunreachable-code"
     /*(void) message_content;*/
-    return false;
+    return true;
 #pragma clang diagnostic pop
 }
