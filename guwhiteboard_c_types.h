08da466ec27306703044178c85f9b3d38d426e95
Carl Lusty <carl.lusty@griffithuni.edu.au>
2018-03-01 16:58:09 +1000

    Add a manual option to the C header includes.

--- guwhiteboard_c_types.h
+++ guwhiteboard_c_types.h
@@ -9,6 +9,8 @@
 #ifndef GUWHITEBOARD_C_TYPES_H
 #define GUWHITEBOARD_C_TYPES_H
 
+#include <gusimplewhiteboard/guwhiteboard_c_types_manual_definitions.h>
+
 
 #include <gusimplewhiteboard/typeClassDefs/wb_top_particles.h>
 #include <gusimplewhiteboard/typeClassDefs/wb_point2d.h>
@@ -16,18 +18,5 @@
 #include <gusimplewhiteboard/typeClassDefs/wb_field_horizon.h>
 #include <gusimplewhiteboard/typeClassDefs/wb_nao_walk_command.h>
 #include <gusimplewhiteboard/typeClassDefs/wb_nao_walk_status.h>
-// ADDED by VLAD for Localizaiton simulator to compile
-#include <gusimplewhiteboard/typeClassDefs/wb_filtered_vision_object_type.h>
-#include <gusimplewhiteboard/typeClassDefs/wb_ball.h>
-#include <gusimplewhiteboard/typeClassDefs/wb_goal.h>
-#include <gusimplewhiteboard/typeClassDefs/wb_goal.h>
-#include <gusimplewhiteboard/typeClassDefs/wb_filtered_arrayonedimobjects.h>
-#include <gusimplewhiteboard/typeClassDefs/wb_filtered_arrayballsightings.h>
-#include <gusimplewhiteboard/typeClassDefs/wb_filtered_vision_object.h>
-#include <gusimplewhiteboard/typeClassDefs/wb_sensors_torso.h>
-#include <gusimplewhiteboard/typeClassDefs/wb_filteredsonarobject.h>
-#include <gusimplewhiteboard/typeClassDefs/wb_arrayoffilteredsonarobjects.h>
-#include <gusimplewhiteboard/typeClassDefs/wb_top_particles.h>
-#include <gusimplewhiteboard/typeClassDefs/wb_hal_headtarget.h>
-#include <gusimplewhiteboard/typeClassDefs/wb_sensors_sonar.h>
+
 #endif //GUWHITEBOARD_C_TYPES_H
---
+++ guwhiteboard_c_types_manual_definitions.h
@@ -0,0 +1,14 @@
+#include <gusimplewhiteboard/typeClassDefs/wb_filtered_vision_object_type.h>
+#include <gusimplewhiteboard/typeClassDefs/wb_ball.h>
+#include <gusimplewhiteboard/typeClassDefs/wb_goal.h>
+#include <gusimplewhiteboard/typeClassDefs/wb_goal.h>
+#include <gusimplewhiteboard/typeClassDefs/wb_filtered_arrayonedimobjects.h>
+#include <gusimplewhiteboard/typeClassDefs/wb_filtered_arrayballsightings.h>
+#include <gusimplewhiteboard/typeClassDefs/wb_filtered_vision_object.h>
+#include <gusimplewhiteboard/typeClassDefs/wb_sensors_torso.h>
+#include <gusimplewhiteboard/typeClassDefs/wb_filteredsonarobject.h>
+#include <gusimplewhiteboard/typeClassDefs/wb_arrayoffilteredsonarobjects.h>
+#include <gusimplewhiteboard/typeClassDefs/wb_top_particles.h>
+#include <gusimplewhiteboard/typeClassDefs/wb_hal_headtarget.h>
+#include <gusimplewhiteboard/typeClassDefs/wb_sensors_sonar.h>
+
